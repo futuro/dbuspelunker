42b59fc6640711796b6902bf019382fe4e50524c
Futuro <evan@cs.umn.edu>
2013-08-08 01:00:18 -0500

    Added introspect function

--- dbuspelunker.lisp
+++ dbuspelunker.lisp
@@ -5,4 +5,9 @@
     (dbus:with-introspected-object (system-obj bus dbus-path-dbus dbus-service-dbus)
       (system-obj dbus-service-dbus "ListNames"))))
 
-#+nil(defun introspect ())
+(defun introspect (path destination)
+  (dbus:with-open-bus (bus (dbus:system-server-addresses))
+    (dbus:invoke-method (dbus:bus-connection bus) "Introspect"
+			:path path
+			:destination destination
+			:interface dbus-interface-introspectable)))
