21f6c022b4e71f563c0907f444319cadc2be660b
Futuro <evan@cs.umn.edu>
2014-01-18 19:24:32 -0600

    Added xml-sxml/lxml parsing via s-xml library
    Added two new function convert-string-to{s,l}xml.
    
    Now I need to consider what a meaningful context would look like for
    dbuspelunker.

--- dbuspelunker.asd
+++ dbuspelunker.asd
@@ -2,7 +2,7 @@
 
 (asdf:defsystem #:dbuspelunker
   :description "DBuSpelunker explores the deep caves of D-Bus on your system."
-  :depends-on (#:dbus)
+  :depends-on (#:dbus #:s-xml)
   :serial t
   :components ((:file "package")
 	       (:file "constants")
--- dbuspelunker.lisp
+++ dbuspelunker.lisp
@@ -7,7 +7,14 @@
 
 (defun introspect (path destination)
   (dbus:with-open-bus (bus (dbus:system-server-addresses))
-    (dbus:invoke-method (dbus:bus-connection bus) "Introspect"
+    (dbus:invoke-method (dbus:bus-connection bus)
+			"Introspect"
 			:path path
 			:destination destination
 			:interface dbus-interface-introspectable)))
+
+(defun convert-string-to-sxml (xml-string)
+  (s-xml:parse-xml-string xml-string :output-type :sxml))
+
+(defun convert-string-to-lxml (xml-string)
+  (s-xml:parse-xml-string xml-string :output-type :lxml))
